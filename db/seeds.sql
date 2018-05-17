366302bd0fb6fab3a53bed69c4016b3e41d118cf
Steve Valasek <svalasek@gmail.com>
2018-05-17 10:13:14 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,42 +2,148 @@ INSERT INTO beerList (brewery_name, brewery_city, beer_name, beer_type)
 VALUES
 ('XII West Brewing Co', 'Gilbert','The Weight', 'Scotch Ale'),
 ('XII West Brewing Co', 'Gilbert','Midnight Run', 'Stout'),
-('XII West Brewing Co', 'Gilbert','Acquisitions', 'Belgian Farmhouse'),
+('XII West Brewing Co', 'Gilbert','Acquisitions', 'Strong Ale'),
 ('XII West Brewing Co', 'Gilbert','Jedi Juice', 'IPA'),
 ('XII West Brewing Co', 'Gilbert','Ethereal', 'Pale Ale'),
 ('XII West Brewing Co', 'Gilbert','Frontside', 'IPA'),
 ('XII West Brewing Co', 'Gilbert','Shwifty', 'Wheat'),
-('XII West Brewing Co', 'Gilbert','Knarbar', 'Barley Wine'),
+('XII West Brewing Co', 'Gilbert','Knarbar', 'Strong Ale'),
 ('XII West Brewing Co', 'Gilbert','K-LAX', 'IPA'),
 ('XII West Brewing Co', 'Gilbert','Super Beaker', 'IPA'),
 ('XII West Brewing Co', 'Gilbert','Blap! Blap', 'Wheat'),
-('XII West Brewing Co', 'Gilbert','Mijo', 'Trippel'),
+('XII West Brewing Co', 'Gilbert','Mijo', 'Strong Ale'),
 ('Arizona Wilderness Brewing', 'Gilbert','ADHD#2', 'Pale Ale'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Boilgeist', 'IPA'),
-('Arizona Wilderness Brewing', 'Gilbert', 'Crystal Eyes', 'DIPA'),
+('Arizona Wilderness Brewing', 'Gilbert', 'Crystal Eyes', 'Strong Ale'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'DHW: Thirsty and Miserable', 'IPA'),
-('Arizona Wilderness Brewing', 'Gilbert', 'DC Mmountain DIPA', 'DIPA'),
-('Arizona Wilderness Brewing', 'Gilbert', 'O-Line IPA', 'Trippel'),
+('Arizona Wilderness Brewing', 'Gilbert', 'DC Mmountain DIPA', 'Strong Ale'),
+('Arizona Wilderness Brewing', 'Gilbert', 'O-Line IPA', 'Strong Ale'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Paiute Pale Ale', 'Pale Ale'),
-('Arizona Wilderness Brewing', 'Gilbert', 'Refuge IPA'),
+('Arizona Wilderness Brewing', 'Gilbert', 'Refuge',' IPA'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Viking Tea Time', 'Lager'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Saddle Mountain', 'Saison'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Saison Sombre', 'Saison'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Salome Saison', 'Saison'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Salt River Saison', 'Saison'),
-('Arizona Wilderness Brewing', 'Gilbert', 'Baboquivari Belgian Blonde', 'Blonde'),
+('Arizona Wilderness Brewing', 'Gilbert', 'Baboquivari Belgian Blonde', 'Blonde Ale'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Klothos Temple', 'IPA'),
-('Arizona Wilderness Brewing', 'Gilbert', 'New Brood', 'Belgian Quad IPA'),
+('Arizona Wilderness Brewing', 'Gilbert', 'New Brood', 'Strong Ale'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Grapefruit Gose', 'Gose'),
 ('Arizona Wilderness Brewing', 'Gilbert', 'Hummingbird Springs', 'Saison'),
 ('Uncle Bears Brewery', 'Phoenix', 'Fence Jumper', 'Ale'),
 ('Uncle Bears Brewery', 'Phoenix', 'Mandarin Wheat', 'Wheat'),
-('Uncle Bears Brewery', 'Phoenix', 'Head to Tale', 'Ale' ),
+('Uncle Bears Brewery', 'Phoenix', 'Head to Tale', 'Ale'),
 ('Uncle Bears Brewery', 'Phoenix', 'Ocean Beach', 'IPA' ),
 ('Uncle Bears Brewery', 'Phoenix', 'Bears Beach Bum', 'IPA'),
-('Uncle Bears Brewery', 'Phoenix', 'The Mutts Nuts', 'DIPA'),
+('Uncle Bears Brewery', 'Phoenix', 'The Mutts Nuts', 'Strong Ale'),
 ('Uncle Bears Brewery', 'Phoenix', 'Barkleys Peanut Butter Cup', 'Porter'),
-('Uncle Bears Brewery', 'Phoenix', 'Wolfhound Irish Red', 'Ale'),
+('Uncle Bears Brewery', 'Phoenix', 'Wolfhound Irish Red', 'Red Ale'),
 ('Uncle Bears Brewery', 'Phoenix', 'Perfecto Resado Cigar Beer', 'Ale'),
 ('Uncle Bears Brewery', 'Phoenix', 'English Bulldog', 'IPA'),
-('Uncle Bears Brewery', 'Phoenix', 'Ruff Night', 'IPA')
+('Uncle Bears Brewery', 'Phoenix', 'Ruff Night', 'IPA'),
+('Four Peaks', 'Tempe','Kilt Lifter', 'Scotch Ale'),
+('Four Peaks', 'Tempe','Hop Knot', 'IPA'),
+('Four Peaks', 'Tempe','Sunbru', 'Kolsch'),
+('Four Peaks', 'Tempe','Oatmeal Stout', 'Stout'),
+('Four Peaks', 'Tempe','Double Knot', 'Strong Ale'),
+('Four Peaks', 'Tempe','Pitchfork', 'Pale Ale'),
+('Four Peaks', 'Tempe','Peach', 'Fruit Beer'),
+('Four Peaks', 'Tempe','8th Street Ale', 'Pale Ale'),
+('Four Peaks', 'Tempe','The Raj', 'IPA'),
+('Four Peaks', 'Tempe','White Ale', 'Witbier'),
+('Sleepy Dog', 'Tempe','Red Rover', 'Red Ale'),
+('Sleepy Dog', 'Tempe','Tail Chaser IPA', 'IPA'),
+('Sleepy Dog', 'Tempe','Wet Snout', 'Stout'),
+('Sleepy Dog', 'Tempe','Peanut Butter Stout', 'Stout'),
+('Sleepy Dog', 'Tempe','Feisty Chihuahua', 'Lager'),
+('Blasted Barley Co', 'Tempe','Orange Bomb', 'Stout'),
+('Blasted Barley Co', 'Tempe','Bang Bang Double Black IPA', 'Strong Ale'),
+('Blasted Barley Co', 'Tempe','A Mountain Amber', 'Red Ale'),
+('Blasted Barley Co', 'Tempe','St. Nobels Mint Chocolate Stout', 'Stout'),
+('Blasted Barley Co', 'Tempe','Greenthumb Session IPA', 'IPA'),
+('Blasted Barley Co', 'Tempe','Blasted IPA', 'IPA'),
+('Blasted Barley Co', 'Tempe','Bro Easy', 'Blonde Ale'),
+('Blasted Barley Co', 'Tempe','Extradynamit', 'Strong Ale'),
+('Blasted Barley Co', 'Tempe','Duke of Dank', 'Strong Ale'),
+('Blasted Barley Co', 'Tempe','Blasted Hefeweizen', 'Wheat'),
+('Blasted Barley Co', 'Tempe','Cloudy Boi', 'IPA'),
+('Blasted Barley Co', 'Tempe','Oh My Guava', 'Fruit Beer'),
+('Blasted Barley Co', 'Tempe','Marlenes Strawberry Cream Dream', 'Cream Ale'),
+('Blasted Barley Co', 'Tempe','Original Sin', 'Strong Ale'),
+('Blasted Barley Co', 'Tempe','Coppersmith', 'Ale'),
+('Boulders On Broadway', 'Tempe','Biere Blanche', 'Witbier'),
+('Boulders On Broadway', 'Tempe','Winter Solstice', 'Amber Ale'),
+('Boulders On Broadway', 'Tempe','Humbolt Brown Hemp Ale', 'Brown Ale'),
+('Boulders On Broadway', 'Tempe','Transatlantique Kriek Lips of Faith 2017', 'Fruit Beer'),
+('Boulders On Broadway', 'Tempe','Bobby Jonesin For Peaches', 'IPA'),
+('Boulders On Broadway', 'Tempe','Puple Haze', 'Fruit Beer'),
+('Boulders On Broadway', 'Tempe','Amber Ale', 'Amber Ale'),
+('Boulders On Broadway', 'Tempe','Candi Stout', 'Stout'),
+('Boulders On Broadway', 'Tempe','Sierra Navada Pale Ale', 'Pale Ale'),
+('Boulders On Broadway', 'Tempe','Black Ops 2017', 'Stout'),
+('Boulders On Broadway', 'Tempe','Dukes of Haze', 'IPA'),
+('Boulders On Broadway', 'Tempe','BHC DIPA', 'Strong Ale'),
+('Boulders On Broadway', 'Tempe','Noche Dulce', 'Porter'),
+('Boulders On Broadway', 'Tempe','All Day IPA', 'IPA'),
+('Boulders On Broadway', 'Tempe','Voodo Ranger Juicy Haze IPA', 'IPA'),
+('Boulders On Broadway', 'Tempe','Betty IPA', 'IPA'),
+('Boulders On Broadway', 'Tempe','Green Zebra', 'Fruit Beer'),
+('Boulders On Broadway', 'Tempe','Kentucky Breakfast Stout', 'Stout'),
+('Boulders On Broadway', 'Tempe','Rubaeus', 'Fruit Beer'),
+('Boulders On Broadway', 'Tempe','Ocean Front Property', 'Pale Ale'),
+('Boulders On Broadway', 'Tempe','Senor Chata', 'Ale'),
+('Boulders On Broadway', 'Tempe','Peanut Butter Milk Stout', 'Stout'),
+('Boulders On Broadway', 'Tempe','S.W.A.I.N Style W/O An Interesting Name', 'IPA'),
+('Boulders On Broadway', 'Tempe','Cali Creamin', 'Cream Ale'),
+('Boulders On Broadway', 'Tempe','Solid Gold', 'American Lager'),
+('Boulders On Broadway', 'Tempe','Juicy Goodnes', 'Golden Ale'),
+('Boulders On Broadway', 'Tempe','Orderville', 'IPA'),
+('Boulders On Broadway', 'Tempe','Brunus Robustus', 'Porter'),
+('OHSO Brewery', 'Gilbert','OHSO Hoppy', 'IPA'),
+('OHSO Brewery', 'Gilbert','Morning Brew', 'Stout'),
+('OHSO Brewery', 'Gilbert','Popcycle Blonde', 'Fruit Beer'),
+('OHSO Brewery', 'Gilbert','Lost Viking', 'Porter'),
+('OHSO Brewery', 'Gilbert','OHSO Brite', 'Pilsner'),
+('OHSO Brewery', 'Gilbert','Orange Pedal', 'Fruit Beer'),
+('OHSO Brewery', 'Gilbert','Handlebar Hefe', 'Wheat'),
+('OHSO Brewery', 'Gilbert','89 Ale', 'Ale'),
+('San Tan Brewing Co', 'Chandler','Devils Ale', 'Pale Ale')
+('San Tan Brewing Co', 'Chandler','HopShock IPA', 'IPA')
+('San Tan Brewing Co', 'Chandler','Mr Pineapple', 'Fruit Beer')
+('San Tan Brewing Co', 'Chandler','San Tan Hefeweizen', 'Wheat')
+('San Tan Brewing Co', 'Chandler','MoonJuice Grapefruit IPA', 'Fruit Beer')
+('San Tan Brewing Co', 'Chandler','MoonJuice IPA', 'IPA')
+('San Tan Brewing Co', 'Chandler','SunSpot Gold', 'Golden Ale')
+('San Tan Brewing Co', 'Chandler','Epicenter Amber', 'Amber Ale')
+('San Tan Brewing Co', 'Chandler','Gordo Stout', 'Stout')
+('San Tan Brewing Co', 'Chandler','LimeLeaf', 'Fruit Beer')
+('San Tan Brewing Co', 'Chandler','San Tan Oktoberfest', 'Lager')
+('San Tan Brewing Co', 'Chandler','Sex Panther', 'Porter')
+('San Tan Brewing Co', 'Chandler','Yotes Pale Ale', 'Pale Ale')
+('San Tan Brewing Co', 'Chandler','Kilohop', 'Strong Ale')
+('San Tan Brewing Co', 'Chandler','Count Hopula', 'Red IPA')
+('San Tan Brewing Co', 'Chandler','Winter Warmer', 'Strong Ale')
+('The Perch Brewery Co', 'Chandler','Joker Amber Ale', 'Amber Ale')
+('The Perch Brewery Co', 'Chandler','Clown Shoes Mango', 'Kolsch')
+('The Perch Brewery Co', 'Chandler','Framboise Rose Gose', 'Gose')
+('The Perch Brewery Co', 'Chandler','Pulp Fusion', 'IPA')
+('The Perch Brewery Co', 'Chandler','The Wit-ty Assistant', 'Witbier')
+('The Perch Brewery Co', 'Chandler','Hatch Chile Gatos', 'Fruit Beer')
+('The Perch Brewery Co', 'Chandler','Old Rasputin', 'Stout')
+('The Perch Brewery Co', 'Chandler','Cutthroat Porter', 'Porter')
+('The Perch Brewery Co', 'Chandler','The Perch Storming the Castle', 'Strong Ale')
+('The Perch Brewery Co', 'Chandler','The Perch Two Cents', 'IPA')
+('The Perch Brewery Co', 'Chandler','The Perch Wit O Honey', 'Witbier')
+('The Perch Brewery Co', 'Chandler','The Perch Bloody Flockn Haze', 'IPA')
+('The Perch Brewery Co', 'Chandler','The Perch Burd Light', 'Pilsner')
+('The Perch Brewery Co', 'Chandler','The Perch Belgian Peach', 'Fruit Beer')
+('The Perch Brewery Co', 'Chandler','The Perch Vaca Milk Stout', 'Stout')
+('The Perch Brewery Co', 'Chandler','The Perch Pockets Pale Ale', 'Pale Ale')
+('The Perch Brewery Co', 'Chandler','The Perch Bird Cafe', 'Brown Ale')
+('The Perch Brewery Co', 'Chandler','The Perch Belgian Blackberry', 'Fruit Beer')
+('The Perch Brewery Co', 'Chandler','The Perch Mango Reverso', 'IPA')
+('The Perch Brewery Co', 'Chandler','The Perch DGS Pink Boots Collab', 'IPA')
+('The Perch Brewery Co', 'Chandler','The Perch Bird Games', 'Fruit Beer')
+('The Perch Brewery Co', 'Chandler','The Perch De Puanu', 'Saison')
+('The Perch Brewery Co', 'Chandler','The Perch People Overwhelm Me', 'IPA')
+('The Perch Brewery Co', 'Chandler','Stiegl Radler Grapefruit Naturtrub', 'Fruit Beer')
