3e2f26a734f59a01d6e326d78edca2cb241b26cc
Steve Valasek <svalasek@gmail.com>
2018-05-18 15:07:00 -0700

    some html/js work

---
+++ public/assets/js/getBeers.js
@@ -0,0 +1,17 @@
+//get beer type from dropdown bar on beerlist
+
+
+
+//.dropdown-item
+
+//GET beer and brewery names from DB where beer type matches.
+
+//creat HTML table of beers of the desired type
+
+//copy pasta for breweries based on city search
+
+//GET breweries/bars by city
+
+//display information
+
+//POST script for adding new beers to DB
