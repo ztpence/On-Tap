5a02b4cf90fea4c3b14bf77bb5ccd0b14abaecbc
Zane P <ztpence@gmail.com>
2018-05-19 11:32:13 -0700

    added jawsdb code to connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,21 @@
 // Setting up mysql connection
 
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'onTap_db',
-    
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'zane123',
+        database: 'onTap_db',
+    });
+};
 
-connection.connect(function(err){
+ connection.connect(function(err){
     if (err){
         console.error('error connecting: ' + err.stack);
         return;
@@ -19,5 +23,7 @@ connection.connect(function(err){
 
     console.log('mysql connected w/ id ' + connection.threadId);
 });
+
+
 
 module.exports = connection;
