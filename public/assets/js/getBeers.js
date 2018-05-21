6b66764f1a4e14a02a798c92deac23a59cb6affd
Steve Valasek <svalasek@gmail.com>
2018-05-21 10:14:02 -0700

    fixed .html pages and potentially JS for making tables

--- public/assets/js/getBeers.js
+++ public/assets/js/getBeers.js
@@ -1,35 +1,43 @@
-// var connection = require('../../config/connection.js');
-// var app = express();
 
-// //replaces the text on the button with the desired beer style (super cool)
-// $(".dropdown-menu li a").click(function(){
-//     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-//     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-//   });
 
-// //get beer type from dropdown bar on beerlist
-// //.dropdown-item
+//get beer type from dropdown bar on beerlist
 
-// $(function(){
-//     $("#beerList").on("click", function(event){
-//       var type = $(this).data("dropdown-item");
-    
 
-// //GET beer and brewery names from DB where beer type matches.
-// app.get("/beerList:type", function(req, res) {
-//     connection.query("SELECT * FROM beerlist where beer_type = ?", [req.params.type], function(err, result) {
-//       var html = "<h1>" + req.params.type + " beers from the East Valley</h1>";
-//   //creat HTML table of beers of the desired type
-//       html += "<ul>";
+$(function(){
+    $("#beerList").on("click", function(event){
+        var beerType = $("#beerList :selcted").text();
+         
+
+//GET beer and brewery names from DB where beer type matches.
+$.ajax("/beerList/" + beerType, {
+  type: "PUT",
+  data: beerType
+}).then(
+  function(){
+
+var html = "<h1>" + req.params.type + " beers from the East Valley</h1>";
+  //creat HTML table of beers of the desired type
+      html += "<ul>";
   
-//       for (var i = 0; i < result.length; i++) {
-//         html += "<li><p>Name: " + result[i].beer_name + "</p>";
-//         html += "<p> from: " + result[i].brewery_name + "</p></li>";
-//       }  
-//       html += "</ul>";
+      for (var i = 0; i < result.length; i++) {
+        html += "<li><p>Name: " + result[i].beer_name + "</p>";
+        html += "<p> from: " + result[i].brewery_name + "</p></li>";
+      }  
+      html += "</ul>";
   
-//       res.send(html);
-//       });
-//     });
-//   });
-// });
+      res.send(html);
+      });
+    });
+
+    //replaces the text on the button with the desired beer style
+    $(".dropdown-menu li a").click(function(){
+      $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+      $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
+
+  });
+
+
+  
+
+
--- public/assets/js/getBrews.js
+++ public/assets/js/getBrews.js
@@ -1,42 +1,37 @@
-// var connection = require('../../config/connection.js');
+var connection = require('../../config/connection.js');
 // var app = express();
 
-// //replaces the text on the button with the desired beer style (super cool)
-// $(".dropdown-menu li a").click(function(){
-//     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-//     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-//   });
-
-// //get beer type from dropdown bar on beerlist
-// //.dropdown-item
-
-// $(function(){
-//     $('.dropdown-toggle').dropdown()
-// })
-
-// //GET beer and brewery names from DB where beer type matches.
-// app.get("/breweryList:city", function(req, res) {
-//     connection.query("SELECT * FROM beerlist where city_name = ? SORT BY brewery_name", [req.params.name], function(err, result) {
-//       var html = "<h1>" + req.params.name + " Bars and Breweries in the East Valley</h1>";
-//   //creat HTML table of beers of the desired type
-//       html += "<ul>";
-  
-//       for (var i = 0; i < result.length; i++) {
-//         html += "<li><p>Name: "+ result[i].brewery_name + "</p></li>";
-//       }  
-//       html += "</ul>";
-  
-//       res.send(html);
-//     });
-//   });
-  
 
 
+//get beer type from dropdown bar on beerlist
+//.dropdown-item
 
-// //copy pasta for breweries based on city search
+$(function(){
+    $("#cityList").on("click", function(event){
+        var citySel = $("#cityList :selected").text();
 
-// //GET breweries/bars by city
+    $.ajax("/cityList/" + citySel, {
+        type: "PUT",
+        data: citySel
+    }).then(
+        function(){
 
-// //display information
-
-// //POST script for adding new beers to DB
+            var html = "<h1>" + req.params.city + " Bars and Breweries in the East Valley</h1>";
+            //creat HTML table of beers of the desired type
+                html += "<ul>";
+            
+                for (var i = 0; i < result.length; i++) {
+                  html += "<li><p>Name: "+ result[i].brewery_name + "</p></li>";
+                }  
+                html += "</ul>";
+            
+                res.send(html);
+              });
+            });
+              
+    //replaces the text on the button with the selected city
+        $(".dropdown-menu li a").click(function(){
+            $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+            $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+        });       
+    });
