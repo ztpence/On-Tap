e6441f0aea62b60415004808aaddfbc6c035ef32
Steve Valasek <svalasek@gmail.com>
2018-05-21 09:37:04 -0700

    Saturday update

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'zane123',
+        password: 'dad8710',
         database: 'onTap_db',
     });
 };
--- public/assets/js/getBrews.js
+++ public/assets/js/getBrews.js
@@ -1,42 +1,42 @@
-// var connection = require('../../config/connection.js');
-// var app = express();
 
-// //replaces the text on the button with the desired beer style (super cool)
-// $(".dropdown-menu li a").click(function(){
-//     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-//     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-//   });
 
-// //get beer type from dropdown bar on beerlist
-// //.dropdown-item
 
-// $(function(){
-//     $('.dropdown-toggle').dropdown()
-// })
 
-// //GET beer and brewery names from DB where beer type matches.
-// app.get("/breweryList:city", function(req, res) {
-//     connection.query("SELECT * FROM beerlist where city_name = ? SORT BY brewery_name", [req.params.name], function(err, result) {
-//       var html = "<h1>" + req.params.name + " Bars and Breweries in the East Valley</h1>";
-//   //creat HTML table of beers of the desired type
-//       html += "<ul>";
+
+//get beer type from dropdown bar on beerlist
+
+
+$(function(){
+    $("#brewList").on("click", function(event){
+      var city = $(this).data("dropdown-item");    
+
+//GET beer and brewery names from DB where beer type matches.
+$.ajax("/brewList/" + city, {
+  type: "PUT",
+  data: city_name
+}).then(
+  function(){
+
+var html = "<h1>" + req.params.data + " are located in Phoenix's East Valley</h1>";
+  //creat HTML table of beers of the desired type
+      html += "<ul>";
   
-//       for (var i = 0; i < result.length; i++) {
-//         html += "<li><p>Name: "+ result[i].brewery_name + "</p></li>";
-//       }  
-//       html += "</ul>";
+      for (var i = 0; i < result.length; i++) {
+        html += "<p> from: " + result[i].brewery_name + "</p></li>";
+      }  
+      html += "</ul>";
   
-//       res.send(html);
-//     });
-//   });
+      res.send(html);
+      });
+    });
+
+    //replaces the text on the button with the desired beer style (super cool)
+    $(".dropdown-menu li a").click(function(){
+      $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+      $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+    });
+
+  });
+
+
   
-
-
-
-// //copy pasta for breweries based on city search
-
-// //GET breweries/bars by city
-
-// //display information
-
-// //POST script for adding new beers to DB
