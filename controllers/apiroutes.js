dbb882df08e977e57ba34d7c1110e5f9bf5adf22
Fabio Restrepo <fabio@Fabios-MacBook-Pro.local>
2018-05-20 18:58:00 -0700

    apiRoute update

--- controllers/apiroutes.js
+++ controllers/apiroutes.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
-router.get("/api", function(req, res) {
+router.get("/api/all", function(req, res) {
     db.beerList.findAll({})
       .then(function(dbBeer) {
         res.json(dbBeer);
