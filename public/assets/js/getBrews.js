d941605fae7d9e10b98401f07cdf69fadb368528
Steve Valasek <svalasek@gmail.com>
2018-05-22 12:20:01 -0700

    tables are working, still geting duplicate beers tho

--- public/assets/js/getBrews.js
+++ public/assets/js/getBrews.js
@@ -1,9 +1,31 @@
 $('#cityList').on('change', function(event){
     var cityVal = ($('#cityList').val());
-    console.log(cityVal);
-    console.log("/api/breweries/" + cityVal);
+    $('#jumbotron').hide();
+    // console.log(cityVal);
+    // console.log("/api/breweries/" + cityVal);
   
     $.get("/api/breweries/" + cityVal, function(data) {
+      //console.log("city data: " + data);
+      $('#cityTable').empty();
+      
+      if (!data) {
+          $('#cityTable').append("<h2> Opps, let's try that again</h2>");
+         
+      }
+      else {               
+          for (var i = 0; i < data.length; i++) {
+  
+              var row = $("<tr>");
+              row.addClass("city");
+        
+              row.append("<td>" + data[i] + "</td>");
+              console.log("19: " + data[i]);
+                           
+              $("#cityTable").append(row);
+        
+            }
+        
+          }
       
     })
   });
